4ddd00fbab628b5e40b0cf037a38b4dfa82340f9
Aayush <theundisputed.ze@gmail.com>
2025-08-10 21:19:26 +0545

    commit

--- apps/streamlit_app/main.py
+++ apps/streamlit_app/main.py
@@ -1,3 +1,5 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 import asyncio
 import streamlit as st
 from src.financial_rag.parsers.parser import MultimodalParser
